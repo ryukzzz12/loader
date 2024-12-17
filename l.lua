e919ff10451f68f81ccee15ac69eeac1ea338bba
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2024-12-17 17:11:22 +0100

    Update l.lua

--- l.lua
+++ l.lua
@@ -1,6 +1,6 @@
 -- Verifica el PlaceId del juego
 if game.PlaceId == 82252715302410 then
-    print("Hola")
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/ryukzzz12/loader/refs/heads/main/swfantasy.lua?token=GHSAT0AAAAAAC4BQ6ZZY564ZD3IR5SV4KQUZ3BUK7Q'))()
 elseif game.PlaceId == 4483381587 then
     print("Hola 2")
 elseif game.PlaceId == 4483381582 then
