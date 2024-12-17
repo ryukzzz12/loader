3f1bd13e10867ba207b9eb25654084ece4baee16
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2024-12-17 17:05:00 +0100

    Create l.lua

---
+++ l.lua
@@ -0,0 +1,10 @@
+-- Verifica el PlaceId del juego
+if game.PlaceId == 82252715302410 then
+    print("Hola")
+elseif game.PlaceId == 4483381587 then
+    print("Hola 2")
+elseif game.PlaceId == 4483381582 then
+    print("Skibidi Toilet")
+else
+    print("ID de lugar no reconocido")
+end
