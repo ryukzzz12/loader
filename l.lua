b462f751c8cc8127241e5aeda65ff4fbd48bcb44
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2024-12-17 17:23:19 +0100

    Update l.lua

--- l.lua
+++ l.lua
@@ -1,7 +1,7 @@
 -- Verifica el PlaceId del juego
 if game.PlaceId == 82252715302410 then ---- sword fantasy
    print("GameDetected: Sword Fantasy")
-   loadstring(game:HttpGet('https://raw.githubusercontent.com/ryukzzz12/loader/refs/heads/main/swfantasy.lua?token=GHSAT0AAAAAAC4BQ6ZZY564ZD3IR5SV4KQUZ3BUK7Q'))()
+   loadstring(game:HttpGet('https://raw.githubusercontent.com/ryukzzz12/loader/refs/heads/main/swfantasy.lua'))()
 elseif game.PlaceId == 4483381587 then
     print("Hola 2")
 elseif game.PlaceId == 4483381582 then
