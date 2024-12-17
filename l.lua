1e8034f29856224fa1ca5756b79a340caaeba442
ryukzzz12 <126538660+ryukzzz12@users.noreply.github.com>
2024-12-17 17:13:20 +0100

    Update l.lua

--- l.lua
+++ l.lua
@@ -1,6 +1,7 @@
 -- Verifica el PlaceId del juego
 if game.PlaceId == 82252715302410 then ---- sword fantasy
    loadstring(game:HttpGet('https://raw.githubusercontent.com/ryukzzz12/loader/refs/heads/main/swfantasy.lua?token=GHSAT0AAAAAAC4BQ6ZZY564ZD3IR5SV4KQUZ3BUK7Q'))()
+   print("loaded succes!!")
 elseif game.PlaceId == 4483381587 then
     print("Hola 2")
 elseif game.PlaceId == 4483381582 then
